c8ae814edea836340850e83319cc3810559fb776
GabrielRicardoXavier <ricardocardosoxavier@gmail.com>
2024-07-02 18:17:54 +0000

    deploy1

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 from saie import app
 
-@app.route('/', methods=['GET', 'POST'])
-@login_required
-def home():
-    if request.method == 'POST':
-        alunos_file = request.files.get('file')
-        if alunos_file:
-            filename, file_extension = os.path.splitext(alunos_file.filename)
-            if file_extension.lower() == '.txt' or file_extension.lower() == '.csv':
-                file_path = os.path.join(UPLOAD_FOLDER, alunos_file.filename)
-                alunos_file.save(file_path)
-                import_alunos_from_file(file_path)
-                return render_template('home.html')
-            else:
-                flash('Apenas (.txt e .csv) são permitidos!', 'alert-danger')
-    return render_template('home.html')
-
 ### rodar App e/ou Site ###
 if __name__ == '__main__':
     app.run(host='110.0.0.1', port=8080, debug=True)
--- routes.py
+++ routes.py
@@ -192,6 +192,21 @@ os.chmod(UPLOAD_FOLDER, 0o777)
 def contato():
     return render_template('contato.html')
 
+@app.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        alunos_file = request.files.get('file')
+        if alunos_file:
+            filename, file_extension = os.path.splitext(alunos_file.filename)
+            if file_extension.lower() == '.txt' or file_extension.lower() == '.csv':
+                file_path = os.path.join(UPLOAD_FOLDER, alunos_file.filename)
+                alunos_file.save(file_path)
+                import_alunos_from_file(file_path)
+                return render_template('home.html')
+            else:
+                flash('Apenas (.txt e .csv) são permitidos!', 'alert-danger')
+    return render_template('home.html')
 
 @app.route('/suporte/FAQ', methods=['GET', 'POST'])
 @login_required
