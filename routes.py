93a083dce0ad34a1d3308ddbbca9a7e3bff3364b
GabrielRicardoXavier <ricardocardosoxavier@gmail.com>
2024-07-02 13:46:22 +0000

    mainpy

--- routes.py
+++ routes.py
@@ -187,23 +187,6 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.chmod(UPLOAD_FOLDER, 0o777)
 
 
-@app.route('/', methods=['GET', 'POST'])
-@login_required
-def home():
-    if request.method == 'POST':
-        alunos_file = request.files.get('file')
-        if alunos_file:
-            filename, file_extension = os.path.splitext(alunos_file.filename)
-            if file_extension.lower() == '.txt' or file_extension.lower() == '.csv':
-                file_path = os.path.join(UPLOAD_FOLDER, alunos_file.filename)
-                alunos_file.save(file_path)
-                import_alunos_from_file(file_path)
-                return render_template('home.html')
-            else:
-                flash('Apenas (.txt e .csv) são permitidos!', 'alert-danger')
-    return render_template('home.html')
-
-
 @app.route('/suporte')
 @login_required
 def contato():
